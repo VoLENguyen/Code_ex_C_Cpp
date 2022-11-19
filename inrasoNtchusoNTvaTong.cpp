b4240dc5e3dcceb2a9e6367b2e840bda52517292
nguyen <volen@MYpc.(none)>
2022-11-19 17:28:42 +0700

    nguyen

---
+++ inrasoNtchusoNTvaTong.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int nt(int n){
+    for(int i = 2; i <= sqrt(n); i++){
+        if(n % i == 0){
+            return 0;
+        }
+        return n > 1;
+    }
+}
+int csntvatong(int n){
+    int sum = 0;
+    while (n)
+    {
+        int r = n % 10;
+        sum += r;
+        if(!nt(r)) return 0;
+        n /= 10;
+    }
+    return nt(sum);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int a,b;
+        scanf("%d%d",&a,&b);
+        int cnt = 0;
+        for(int i = a; i <=b; i++){
+            if(csntvatong(i)&&nt(i))
+                cnt++;
+        }
+        printf("%d\n",cnt);
+    }
+}
